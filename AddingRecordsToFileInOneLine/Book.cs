77ead8ea2fb0c6487491a44e18c2520d84eb8cb8
kenan <kenan@LAPTOP-5KTTCAE1>
2022-01-23 13:27:28 +0100

    fixed bugs

---
+++ AddingRecordsToFileInOneLine/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddingRecordsToFileInOneLine
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+
+        public Book()
+        {
+            Title = " N/A";
+            Author = "N/A";
+            Year = 1900;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title,-20} {Author,20} {Year}";
+        }
+
+
+    }
+}
---
+++ AddingRecordsToFileInOneLine/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddingRecordsToFileInOneLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\Addgood.txt";
+
+            List<Book> booksFromMethod = new List<Book>();
+            booksFromMethod = PopulateBooks();
+            WriteToFile(booksFromMethod, path);
+
+
+            ////Add new record
+            Book newBook = new Book();
+
+            newBook.Title = "Sql Databases,";
+            newBook.Author = "Olga,";
+            newBook.Year = 2021;
+            booksFromMethod.Add(newBook);
+
+            ////Add another new record
+            Book newBook1 = new Book();
+            newBook1.Title = "C# design patterns,";
+            newBook1.Author = "Serap,";
+            newBook1.Year = 2021;
+            booksFromMethod.Add(newBook1);
+
+            WriteToFile(booksFromMethod, path);
+
+
+            // show items as string
+            //List<string> booksFromFile = new List<string>();
+            //booksFromFile = ReadAndDisplayAsString(path);
+
+            //foreach (var item in booksFromFile)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+            List<Book> booksFromFile = new List<Book>();
+            booksFromFile = ReadFromFile(path);
+            foreach (var item in booksFromFile)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+        static List<Book> PopulateBooks()
+        {
+            List<Book> books = new List<Book>();
+
+            books.Add(new Book { Title = "Title 3,", Author = "Kenan,", Year = 2010 }); // without coma is not working problem Title 3
+            books.Add(new Book { Title = "Title 4,", Author = "Anass,", Year = 2000 });
+            books.Add(new Book { Title = "Title 1,", Author = "Fatih,", Year = 2003 });
+            books.Add(new Book { Title = "Title 2,", Author = "Florian,", Year = 2008 });
+            books.Add(new Book { Title = "Title 5,", Author = "Wouter,", Year = 2015 });
+
+            return books;
+        }
+
+        static void WriteToFile(List<Book> books, string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+
+                for (int i = 0; i < books.Count; i++) 
+                {
+                    sw.Write(books[i].Title);
+                    sw.Write(books[i].Author);
+                    sw.WriteLine(books[i].Year);
+                }
+            }
+        }
+
+        public static List<Book> ReadFromFile(string path)
+        {
+            List<Book> books = new List<Book>();
+            List<string> lines = File.ReadAllLines(path).ToList();
+
+            foreach (var item in lines)
+            {
+                string[] field = item.Split(',');
+                Book book = new Book();
+
+                book.Title = field[0];
+                book.Author = field[1];
+                book.Year = Convert.ToInt32(field[2]);
+
+                books.Add(book);
+
+            }
+            return books;
+        }
+
+        static List<string> ReadAndDisplayAsString(string path) // order very important
+        {
+            List<string> booksAsString = new List<string>();
+            FileInfo file = new FileInfo(path);
+            int total = 0;
+            using (StreamReader sr = file.OpenText())
+            {
+                string line = string.Empty;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    booksAsString.Add(line);
+                }
+                return booksAsString;
+            }
+        }
+    }
+}
